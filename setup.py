f3da3c7274e012a4d53e8107da29352f174c13da
Vinayak Mehta <vmehta94@gmail.com>
2021-07-17 00:35:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,41 @@
-from setuptools import setup, Extension
+import os
+
+from setuptools import setup, Extension, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+
+with open(os.path.join(here, "src", "example", "__version__.py"), "r") as f:
+    exec(f.read(), about)
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+ext_modules = [
+    Extension("example", sources=[os.path.join("src", "example", "example.c")])
+]
 
 setup(
-    ext_modules=[
-        Extension("example", sources=["src/example.c"]),
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url=about["__url__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    license=about["__license__"],
+    packages=find_packages(where="src", exclude=("tests",)),
+    package_dir={"": "src"},
+    ext_modules=ext_modules,
+    include_package_data=True,
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
